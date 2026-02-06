4f4fd0caf37a7959dc7b76e2e3c7098de1b57fd7
Aman Raj <gopalraj73334@gmail.com>
2026-02-06 11:26:04 +0530

    Done

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: '/backend/:path*',
-                destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+                destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/:path*`,
             },
         ]
     },
